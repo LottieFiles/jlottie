6440ca382c65f62c1224614b3d3625dc230ecd3b
jawish <jawish@gmail.com>
2021-06-03 22:46:42 +0000

    Deploying to gh-pages from  @ 08996c359194f9b212514ca5d6ec713a994ef481 🚀

---
+++ jlottie.umd.js
@@ -0,0 +1,5 @@
+/*!
+ * @lottiefiles/jlottie v1.0.0
+ */
+!function(t){"function"==typeof define&&define.amd?define(t):t()}((function(){"use strict";exports.loadAnimation="jlottie.loadAnimation",exports.goToAndStop="jlottie.goToAndStop",exports.play="jlottie.play",exports.stop="jlottie.stop",exports.destroy="jlottie.destroy"}));
+//# sourceMappingURL=jlottie.umd.js.map
