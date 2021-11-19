56517b24c8494af2d773004c6bcc32702dfa1604
Shaafiee <shaafiee@gmail.com>
2021-11-19 19:12:48 +0800

    working on positioning

--- src/jlottie.js
+++ src/jlottie.js
@@ -790,12 +790,12 @@ export function addGroupPositionTransform(
       transforms.translateY = posY;
     //}
     if (objectId.hasOwnProperty('_anchorX')) {
-      transforms.translate = `translate(${transforms.translateX - transforms.anchorX + transforms.paddingAnchorX},${
-        transforms.translateY - transforms.anchorY + transforms.paddingAnchorY
+      transforms.translate = `translate(${(transforms.translateX - transforms.anchorX) - transforms.paddingAnchorX},${
+        (transforms.translateY - transforms.anchorY) - transforms.paddingAnchorY
       }) `;
     } else {
-      transforms.translate = `translate(${transforms.translateX + (transforms.paddingAnchorX + transforms.paddingX)},${
-        transforms.translateY + (transforms.paddingAnchorY + transforms.paddingY)
+      transforms.translate = `translate(${transforms.translateX - (transforms.paddingAnchorX + transforms.paddingX)},${
+        transforms.translateY - (transforms.paddingAnchorY + transforms.paddingY)
       }) `;
     }
     if (!preTranslate) {
@@ -806,11 +806,11 @@ export function addGroupPositionTransform(
   if (!transforms.isTranslate && transforms.scaled) {
     if (objectId.hasOwnProperty('_anchorX')) {
       transforms.translate = `translate(${(transforms.translateX - transforms.anchorX) - transforms.paddingAnchorX},${
-        (transforms.translateY - transforms.anchorY) - (transforms.paddingX + transforms.paddingAnchorY)
+        (transforms.translateY - transforms.anchorY) - transforms.paddingAnchorY
       }) `;
     } else {
-      transforms.translate = `translate(${transforms.translateX + transforms.paddingAnchorX + transforms.paddingX},${
-        transforms.translateY + transforms.paddingAnchorY + transforms.paddingY
+      transforms.translate = `translate(${transforms.translateX - (transforms.paddingAnchorX + transforms.paddingX)},${
+        transforms.translateY - (transforms.paddingAnchorY + transforms.paddingY)
       }) `;
     }
     /*(transforms.translate = `translate(${transforms.translateX - transforms.paddingAnchorX},${
